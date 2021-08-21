c3036f7bd39d75d2dd8ad42211351fdb9c48b752
Rayhan Sheikh <rsheikh201031@mscse.uiu.ac.bd>
2021-08-21 16:14:58 +0600

    js file update

--- mac-cart.js
+++ mac-cart.js
@@ -1,46 +1,81 @@
-{/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script> */}
 
 
-        
-        document.getElementById('primary-memory').addEventListener('click', function(){
-             const primaryMemory = document.getElementById('memory-cost');
-             primaryMemory.innerText = 0;
-            
+// function for cost
 
-            
-        });
-        document.getElementById('extra-memory').addEventListener('click', function(){
-             const extraMemory = document.getElementById('memory-cost');
-             extraMemory.innerText = 180;
-            
-            
-        });
-        document.getElementById('primary-storage').addEventListener('click', function(){
-             const primaryStorage = document.getElementById('storage-cost');
-             primaryStorage.innerText = 0;
-            
-        });
-        document.getElementById('second-storage').addEventListener('click', function(){
-             const secondStorage = document.getElementById('storage-cost');
-             secondStorage.innerText = 180;
-            
-        });
-        document.getElementById('extra-storage').addEventListener('click', function(){
-             const extraStorage = document.getElementById('storage-cost');
-             extraStorage.innerText = 360;
-            
-        });
-        document.getElementById('normal-delivery').addEventListener('click', function(){
-             const normalDelivery = document.getElementById('delivery-cost');
-             normalDelivery.innerText = 0;
-            
-        });
+function costUpdate(size,number){
+     const storageInput = document.getElementById(size + '-cost');
+     storageInput.innerText = number;
 
-        document.getElementById('fast-delivery').addEventListener('click', function(){
-             const fastDelivery = document.getElementById('delivery-cost');
-             fastDelivery.innerText = 20;
-            
-        });
-       
+    calculateTotal();
+};
 
+// memory cost
+
+document.getElementById('primary-memory').addEventListener('click',function(){
+     costUpdate('memory',0);
+});
+
+document.getElementById('extra-memory').addEventListener('click',function(){
+     costUpdate('memory',180);
+});
+
+// storage cost
+
+document.getElementById('primary-storage').addEventListener('click',function(){
+     costUpdate('storage',0);
+});
+
+document.getElementById('second-storage').addEventListener('click',function(){
+     costUpdate('storage',180);
+});
+
+document.getElementById('extra-storage').addEventListener('click',function(){
+     costUpdate('storage',360);
+});
+
+// delivery cost
+
+document.getElementById('normal-delivery').addEventListener('click',function(){
+     costUpdate('delivery',0);
+});
+document.getElementById('fast-delivery').addEventListener('click',function(){
+     costUpdate('delivery',20);
+});
+
+// get input value
+
+function getInputCost(size){
+     const storageInput = document.getElementById(size + '-cost');
+     const costTotal = parseInt(storageInput.innerText);
+     return costTotal;
+};
+// calculate total
+
+function calculateTotal(){
+      const bestPrice = document.getElementById('best-price');
+     const bestCost = parseInt(bestPrice.innerText);
+     const memoryTotal = getInputCost('memory');
+     const storageTotal = getInputCost('storage');
+     const deliveryTotal = getInputCost('delivery');
+     const totalPrice = bestCost + memoryTotal + storageTotal + deliveryTotal;
+     document.getElementById('total-price').innerText = totalPrice;
+      document.getElementById('total').innerText = totalPrice;
+    
+     
+};
+
+// function for apply promo code
+function discount(name){
+     const promoCode = document.getElementById('promo-code');
+     const totalPrice = document.getElementById('total').innerText;
+     if(promoCode.value == name){
+          const subTotal = parseInt(totalPrice);
+          const discount = subTotal/5; 
+          const grandTotal = totalPrice - discount;
+           document.getElementById('total').innerText = grandTotal;
+     };
+};
+
+document.getElementById('code-submit').addEventListener('click', function(){
+     discount('stevekaku');
+});
